2cb773808140b48527f9a37daaccf841a686544d
clement <clement.musset511@orange.fr>
2025-02-26 10:19:03 +0100

    Init Projet back

---
+++ PasswordManager/Domain/Entities/Application.cs
@@ -0,0 +1,11 @@
+﻿namespace PasswordManager.Domain.Entities
+{
+    public class Application
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public string Type { get; set; } = string.Empty; // "Grand public" ou "Professionnelle"
+
+        public ICollection<PasswordEntry> Passwords { get; set; } = new List<PasswordEntry>();
+    }
+}
---
+++ PasswordManager/Infrastructure/Data/AppDbContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PasswordManager.Domain.Entities;
+
+
+namespace PasswordManager.Infrastructure.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public DbSet<Application> Applications { get; set; }
+        public DbSet<PasswordEntry> PasswordEntries { get; set; }
+
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Application>()
+                .HasMany(a => a.Passwords)
+                .WithOne(p => p.Application)
+                .HasForeignKey(p => p.ApplicationId);
+        }
+    }
+}
